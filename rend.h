3049c299975305367e5dcfb37b64da61011d3248
gukalex <guk.alex.v@gmail.com>
2022-10-21 01:46:27 +0300

    textures + shaders 1/x

--- rend.h
+++ rend.h
@@ -47,9 +47,16 @@ inline v2 operator/(v2 r, float f) { return { r.x / f, r.y / f }; };
 
 struct GLFWwindow;
 
+struct draw_data { // todo: multiple
+    u32 prog; // todo: index and not direct opengl handle?
+    u32 tex;  //
+};
+
 struct rend {
     int w, h;
-    bool vsync; // init only
+    bool vsync;            // init only
+    int max_progs = 32;    // 
+    int max_textures = 32; //
     GLFWwindow* window;
     i64 curr_time; // time, nanosec
     i64 prev_time;
@@ -63,17 +70,23 @@ struct rend {
         void main() {
            gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
            vAttr = aAttr; 
-        }
-    )";
+        })";
     const char* fs_quad = R"(
         #version 450 core
         in vec4 vAttr;
         out vec4 FragColor;
         void main() {
            FragColor = vAttr;
-        }
-    )";
-    u32 prog_quad;
+        })";
+    const char* fs_quad_tex = R"(
+        #version 450 core
+        in vec4 vAttr;
+        out vec4 FragColor;
+        uniform sampler2D rend_t0;
+        void main() {
+            vec2 uv = vAttr.xy;
+            FragColor.rgba = texture(rend_t0, uv).rgba;
+        })";
     u32 vb_quad;
     u32 sb_quad_pos;
     u32 sb_quad_attr1;
@@ -83,11 +96,23 @@ struct rend {
     float* quad_attr1; // vec4
     u32* quad_indices; // 2 triangles = 6
     u32 curr_quad_count = 0;
+    draw_data default_quad_data; // first shader, no textures
+
+    u32 *progs;
+    int curr_progs;
+    u32 *textures;
+    int curr_tex;
 
     void init();
+    u32 shader(const char* vs, const char* fs); // todo: error string
+    u32 texture(u8* data, int w, int h, int channel_count = 4);
+    u32 texture(const char* filename);
     void cleanup();
+    void submit(draw_data data);
     void present();
     bool closed();
     void clear(v4 c);
     void quad(v2 lb, v2 rt, v4 c); // 0-1
+    void quad_a(v2 lb, v2 rt, v4 attr[4]);
+    void quad_t(v2 lb, v2 rt, v2 attr);
 };
