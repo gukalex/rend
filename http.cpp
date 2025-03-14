1a75f9e4a9fd5481d817d22ddba53f3e413bea5d
gukalex <guk.alex.v@gmail.com>
2025-03-14 17:05:25 +0100

    WIP http.cpp

--- http.cpp
+++ http.cpp
@@ -34,18 +34,45 @@ void start_server(const char* host, int port, int count, server_callback *callba
         for (int i = 0; i < count; i++) {
             switch (callbacks[i].type) {
             case REQUEST_GET:
-                srv.Get(callbacks[i].endpoint, [=](const httplib::Request&, httplib::Response& res) {
-                    callbacks[i].callback(&res, 0, 0); // -> res.set_content(data, size, content_type);
+                srv.Get(callbacks[i].endpoint, [=](const httplib::Request& req, httplib::Response& res) {
+                    {
+                        print("remote addr %s (%d), local addr %s (%d)", req.remote_addr.c_str(), req.remote_port, req.local_addr.c_str(), req.local_port);
+                    }
+                    switch (callbacks[i].callback_type) {
+                    case 0:
+                        callbacks[i].callback(&res, 0, 0); break;
+                    case 1:
+                        callbacks[i].callback2(&res, 0, 0, req.remote_addr.c_str(), req.remote_port); break;
+                    default:
+                        ASSERT(false && "wrong callback type");
+                    };
                 });
                 break;
             case REQUEST_POST:
                 srv.Post(callbacks[i].endpoint, [=](const httplib::Request& req, httplib::Response& res) {
+                    {
+                        print("remote addr %s (%d), local addr %s (%d)", req.remote_addr.c_str(), req.remote_port, req.local_addr.c_str(), req.local_port);
+                    }
                     if (req.files.size()) {
                         const char* data = (*req.files.begin()).second.content.data();
                         u64 size = (*req.files.begin()).second.content.size();
-                        callbacks[i].callback(&res, data, size);
+                        switch (callbacks[i].callback_type) {
+                        case 0:
+                            callbacks[i].callback(&res, data, size); break;
+                        case 1:
+                            callbacks[i].callback2(&res, data, size, req.remote_addr.c_str(), req.remote_port); break;
+                        default:
+                            ASSERT(false && "wrong callback type");
+                        };
                     } else {
-                        callbacks[i].callback(&res, 0, 0);
+                        switch (callbacks[i].callback_type) {
+                        case 0:
+                            callbacks[i].callback(&res, 0, 0); break;
+                        case 1:
+                            callbacks[i].callback2(&res, 0, 0, req.remote_addr.c_str(), req.remote_port); break;
+                        default:
+                            ASSERT(false && "wrong callback type");
+                        };
                     }
                 });
                 break;
@@ -53,7 +80,8 @@ void start_server(const char* host, int port, int count, server_callback *callba
         }
         srv.set_keep_alive_max_count(INT_MAX);
         srv.set_keep_alive_timeout(INT_MAX);
-        srv.listen(host, port);
+        bool ls = srv.listen(host, port);
+        print("listen res %d", ls);
     });
     srv_thread.detach();
 }
